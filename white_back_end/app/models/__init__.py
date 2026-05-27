0231693c992ee67b3c068947e43bc8aba9341be2
remi_w_-admin <scarletremilia092@gmail.com>
2026-05-27 17:33:23 +0800

    feat(chat)：数据库迁移，增加消息会话模块所需的三张关系表，后端新建 ORM 模型并补全建库脚本

--- white_back_end/app/models/__init__.py
+++ white_back_end/app/models/__init__.py
@@ -1,4 +1,5 @@
 from .coupon import Coupon, UserCoupon
+from .chat import Conversation, ConversationMember, Message
 from .order import Order
 from .order_status import OrderStatus
 from .payment import Payment
@@ -7,9 +8,12 @@ from .user import User
 from .vehicle import Vehicle
 
 __all__ = [
+    "Conversation",
+    "ConversationMember",
     "Coupon",
     "DriverAverageRating",
     "DriverRating",
+    "Message",
     "Order",
     "OrderStatus",
     "Payment",
